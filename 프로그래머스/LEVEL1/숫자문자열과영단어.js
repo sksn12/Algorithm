8f077631246c4a9b14d99b6e7a0077305851f58b
youngman <sksn12@naver.com>
2021-07-22 14:30:17 +0900

    7월22일

---
+++ 프로그래머스/LEVEL1/숫자문자열과영단어.js
@@ -0,0 +1,35 @@
+function solution(s) {
+  let answer = "";
+  let box = s.split("");
+  const number = [
+    { name: 0, value: "zero" },
+    { name: 1, value: "one" },
+    { name: 2, value: "two" },
+    { name: 3, value: "three" },
+    { name: 4, value: "four" },
+    { name: 5, value: "five" },
+    { name: 6, value: "six" },
+    { name: 7, value: "seven" },
+    { name: 8, value: "eight" },
+    { name: 9, value: "nine" },
+  ];
+  const length = box.length;
+  let val = "";
+  for (let i = 0; i < length; i++) {
+    if (parseInt(box[i])) {
+      answer += box[i];
+    } else {
+      val += box[i];
+      for (let x = 0; x < number.length; x++) {
+        if (number[x].value == val) {
+          answer += number[x].name;
+          val = "";
+        }
+      }
+    }
+  }
+  answer = parseInt(answer);
+
+  return answer;
+}
+console.log(solution("zerozerozero"));
