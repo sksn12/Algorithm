a0ce62765cd9731f8721ca161d9dee5a9e3bdb09
youngman <sksn12@naver.com>
2021-12-24 01:20:58 +0900

    프린터문제 풀기 및 다른사람코드 추가

--- 프로그래머스/LEVEL2/프린터.js
+++ 프로그래머스/LEVEL2/프린터.js
@@ -1,21 +1,48 @@
 function solution(priorities, location) {
-  var list = priorities.map((t, i) => ({
-    my: i === location,
-    val: t,
-  }));
+  let now_location = location;
+  let box = priorities;
+  let cnt = 0;
 
-  var count = 0;
   while (true) {
-    var cur = list.splice(0, 1)[0];
-    console.log(cur);
-    if (list.some((t) => t.val > cur.val)) {
-      list.push(cur);
+    let nowNum = box.shift();
+    if (box.filter((e) => e > nowNum).length > 0) {
+      box.push(nowNum);
+      if (now_location == 0) {
+        now_location = box.length - 1;
+      } else {
+        now_location -= 1;
+      }
     } else {
-      count++;
-      if (cur.my) return count;
+      if (now_location == 0) {
+        cnt++;
+        return cnt;
+      } else {
+        now_location -= 1;
+        cnt++;
+      }
     }
   }
 }
 
-// console.log(solution([2, 1, 3, 2], 2));
-console.log(solution([1, 1, 9, 1, 1, 1], 2));
+// 다른 풀이 some함수와 객체 이용에 유의깊게 보자!
+// function solution(priorities, location) {
+//   var list = priorities.map((t, i) => ({
+//     my: i === location,
+//     val: t,
+//   }));
+//   var count = 0;
+
+//   while (true) {
+//     var cur = list.splice(0, 1)[0];
+//     //some 함수로 list중 cur의 val과 비교해서 하나라도 큰값이 있으면 if true
+//     if (list.some((t) => t.val > cur.val)) {
+//       list.push(cur);
+//     } else {
+//       count++;
+//       if (cur.my) return count;
+//     }
+//   }
+// }
+
+console.log(solution([2, 1, 3, 2], 2));
+// console.log(solution([1, 1, 9, 1, 1, 1], 0));
